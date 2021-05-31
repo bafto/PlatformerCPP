4d0e07e0a26c7905160b690f48f4ef297b94bf7b
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-05-31 09:49:57 +0200

    added Tilemap, not tested yet

---
+++ src/include/LevelRelated/Tilemap.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "TextureMap.h"
+
+class Tile
+{
+public:
+	Tile(sf::Vector2f pos, int tileID, sf::Texture* tex = nullptr);
+
+	void render(sf::RenderTarget& target);
+
+	int GetTileID() const { return TileID; }
+
+public:
+	static inline const sf::Vector2f TileSize = sf::Vector2f(50.f, 50.f);
+
+	sf::RectangleShape rect;
+	sf::Texture* texture;
+	bool inHitbox;
+
+private:
+	int TileID;
+};
+
+class Tilemap
+{
+public:
+	Tilemap();
+	Tilemap(const std::vector<std::string>& lines);
+
+	void Initialize(const std::vector<std::string>& lines);
+
+	void render(sf::RenderTarget& target);
+private:
+	void LoadTextures();
+	void MakeHitboxes();
+
+public:
+	Tile GetTileAtPos(sf::Vector2f pos);
+	bool Collides(const sf::FloatRect& rect);
+	
+public:
+	std::vector<std::vector<Tile>> tiles;
+	std::vector<sf::FloatRect> hitboxes;
+	int width, height;
+
+private:
+	TextureMap texMap;
+
+};
---
+++ src/source/LevelRelated/Tilemap.cpp
@@ -0,0 +1,225 @@
+#include "../../include/LevelRelated/Tilemap.h"
+
+Tile::Tile(sf::Vector2f pos, int tileID, sf::Texture* tex)
+	:
+	TileID(tileID),
+	texture(tex),
+	inHitbox(false)
+{
+	rect.setPosition(pos);
+	rect.setSize(TileSize);
+	if (tex)
+		rect.setTexture(tex);
+	else
+		rect.setFillColor(sf::Color::Transparent);
+}
+
+void Tile::render(sf::RenderTarget& target)
+{
+	if (TileID != 0)
+		target.draw(rect);
+}
+
+Tilemap::Tilemap()
+    :
+    width(0),
+    height(0)
+{}
+
+Tilemap::Tilemap(const std::vector<std::string>& lines)
+{
+	Initialize(lines);
+}
+
+void Tilemap::Initialize(const std::vector<std::string>& lines)
+{
+	texMap.Initialize("assets\\Levels\\" + lines[1]);
+
+	height = lines.size() - 2;
+	width = lines[2].length();
+
+	for (auto& vec : tiles)
+		vec.clear();
+	tiles.clear();
+	tiles.reserve(width);
+	for (auto& vec : tiles)
+		vec.reserve(height);
+
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			sf::Vector2f WorldPos(x * Tile::TileSize.x, y * Tile::TileSize.y);
+			char ch = lines[lines.size() - height + y][x];
+			int tileID = std::atoi(&ch);
+
+			tiles[x][y] = Tile(WorldPos, tileID);
+		}
+	}
+	LoadTextures();
+	MakeHitboxes();
+}
+
+void Tilemap::LoadTextures()
+{
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int tileID = tiles[x][y].GetTileID();
+
+            bool topAir = y == 0 || tiles[x][y - 1].GetTileID() == 0;
+            bool leftAir = x == 0 || tiles[x - 1][y].GetTileID() == 0;
+            bool rightAir = x == width - 1 || tiles[x + 1][y].GetTileID() == 0;
+            bool bottomAir = y == height - 1 || tiles[x][y + 1].GetTileID() == 0;
+
+            if (topAir && bottomAir)
+            {
+                if (leftAir)
+                {
+                    if (rightAir)
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][15];
+                    }
+                    else
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][12];
+                    }
+                }
+                else if (rightAir)
+                {
+                    tiles[x][y].texture = &texMap.textures[tileID][14];
+                }
+                else
+                {
+                    tiles[x][y].texture = &texMap.textures[tileID][13];
+                }
+            }
+            else
+            {
+                if (topAir)
+                {
+                    if (leftAir)
+                    {
+                        if (rightAir)
+                        {
+                            tiles[x][y].texture = &texMap.textures[tileID][3];
+                        }
+                        else
+                        {
+                            tiles[x][y].texture = &texMap.textures[tileID][0];
+                        }
+                    }
+                    else if (rightAir)
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][2];
+                    }
+                    else
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][1];
+                    }
+                }
+                else if (bottomAir)
+                {
+                    if (leftAir)
+                    {
+                        if (rightAir)
+                        {
+                            tiles[x][y].texture = &texMap.textures[tileID][11];
+                        }
+                        else
+                        {
+                            tiles[x][y].texture = &texMap.textures[tileID][8];
+                        }
+                    }
+                    else if (rightAir)
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][10];
+                    }
+                    else
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][9];
+                    }
+                }
+                else
+                {
+                    if (leftAir)
+                    {
+                        if (rightAir)
+                        {
+                            tiles[x][y].texture = &texMap.textures[tileID][7];
+                        }
+                        else
+                        {
+                            tiles[x][y].texture = &texMap.textures[tileID][4];
+                        }
+                    }
+                    else if (rightAir)
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][6];
+                    }
+                    else
+                    {
+                        tiles[x][y].texture = &texMap.textures[tileID][5];
+                    }
+                }
+            }
+        }
+    }
+}
+
+void Tilemap::MakeHitboxes()
+{
+    //loop to each Tile
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            //check if it is solid, and if it isn't already involved in a Hitbox
+            if (tiles[x][y].GetTileID() != 0 && !tiles[x][y].inHitbox)
+            {
+                sf::FloatRect newHitbox = sf::FloatRect(tiles[x][y].rect.getPosition(), tiles[x][y].rect.getSize()); //this will become the finished hitbox
+                tiles[x][y].inHitbox = true; //the tile will definitely be in a hitbox, even if it is alone
+                bool firstLoop = true, breaked = false; //temporary bools for later
+                int xEnd = 0; //the X-Coordinate to determine where the Hitbox will end
+                for (int yy = y; yy < height; yy++) //loop from top to bottom starting at the detected Tile (which isn't already in a Hitbox and is non-solid)
+                {
+                    int xx = x + 1; //declared here to determine the width of the hitbox later
+                    if (firstLoop) //in the first loop, the width of the hitbox is determined so it starts one Tile to the right of the original one
+                    {
+                        for (; xx < width && tiles[xx][yy].GetTileID() != 0 && !tiles[xx][yy].inHitbox; xx++) //loop from left to right until there is a air Tile
+                        {
+                            newHitbox.width += Tile::TileSize.x; //there is no air, so make the hitbox greater
+                            tiles[xx][yy].inHitbox = true; //and set the added Tile to be in a hitbox
+                        }
+                        xEnd = xx; //the right end of the hitbox is set
+                        firstLoop = false;
+                    }
+                    else
+                    {
+                        for (xx = x; xx < xEnd && tiles[xx][yy].GetTileID() != 0 && !tiles[xx][yy].inHitbox; xx++) //we know the width, now we start at the tiles directly under the original one
+                        {
+                            tiles[xx][yy].inHitbox = true; //just add them to the hitbox, the width of it is already set correctly
+                        }
+                    }
+                    if (xx != xEnd) //did we reach the full width of the hitbox?
+                    {
+                        newHitbox.height -= Tile::TileSize.y; //no, so we don't have to increase the height
+                        breaked = true; //we had to end cause it was no perfect rectangle anymore
+                        for (int i = x; i < xx; i++) // those hitboxes where added, but now we have to remove them
+                        {
+                            tiles[i][yy].inHitbox = false;
+                        }
+                        break;
+                    }
+                    newHitbox.height += Tile::TileSize.y; //we reached the width of the hitbox so we increase the height
+                }
+                if (!breaked)
+                {
+                    newHitbox.height -= Tile::TileSize.y; //it wasn't a perfect rectangle so we decrease the height
+                }
+                hitboxes.push_back(newHitbox); //just add the Hitbox
+            }
+        }
+    }
+}
